168c61aaa515a3f4fabfc4623de7ca16ce16fb26
LJoak <ljoakhastor98281@gmail.com>
2022-11-26 04:18:50 +0800

    fix[footer]: text alignment in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -56,6 +56,7 @@ export default function Footer() {
           display: 'flex',
           flexDirection: 'column',
           alignItems: 'end',
+          textAlign: 'right',
         }}
       >
         <Link
