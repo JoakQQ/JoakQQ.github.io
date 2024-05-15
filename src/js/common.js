0627b5f23fccb417862c86a7e3be974e670b0072
JoakQQ <JoakQQ@users.noreply.github.com>
2024-05-15 09:52:15 +0000

    deploy: 8ae98e5ae3eed65b1bc6f472b3dbacfcff870082

--- src/js/common.js
+++
@@ -1,4 +0,0 @@
-import '../css/common.css'
-
-console.log("Hello World!");
-console.log("lole", s)
--- webpack.mapping.js
+++
@@ -1,9 +0,0 @@
-const JS_DIR = './src/js'
-// const CSS_DIR = './src/css'
-
-module.exports = {
-	common: [
-		`${JS_DIR}/common.js`
-		// `${CSS_DIR}/common.css`
-	]
-};
